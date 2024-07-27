353dee9ec223512cd71f4b3667d2b0ebb7483988
ashan <uhashan17929@gmail.com>
2024-07-27 13:10:22 +0530

    Update Experts

--- src/Components/Experts.jsx
+++ src/Components/Experts.jsx
@@ -8,15 +8,15 @@ const experts = [
     company: "TITANCode",
     description: "I oversee day-to-day operations, ensure financial accuracy, and manage budgets to support our team's success and efficiency.",
     positionStyle: { top: '50%', left: '20%' },
-    boxPosition: { top: '5%', left: '10%' }
+    boxPosition: { top: '57%', left: '10%' }
   },
   {
-    name: "Expert 2",
-    position: "Position 2",
+    name: "Samindu Kusalya",
+    position: "Founder & CEO, ",
     company: "TITANCode",
-    description: "Description for expert 2.",
+    description: "As CEO and Founder of TITANcode, I lead our team with a focus on innovation and excellence, ensuring we deliver top-notch tech solutions and drive our mission forward.",
     positionStyle: { top: '50%', left: '38%' },
-    boxPosition: { top: '60%', left: '28%' }
+    boxPosition: { top: '55%', left: '28%' }
   },
   {
     name: "Ashan Kavindu",
@@ -24,23 +24,23 @@ const experts = [
     company: "TITANCode",
     description: "As CEO and Founder of TITANcode, I lead our team with a focus on innovation and excellence, ensuring we deliver top-notch tech solutions and drive our mission forward.",
     positionStyle: { top: '45%', left: '50%' },
-    boxPosition: { top: '5%', left: '50%' }
+    boxPosition: { top: '59%', left: '28%' }
   },
   {
     name: "Sahan Udara",
     position: "Co-Founder & Senior Software Enginner",
     company: "TITANCode",
-    description: "As a Co-Founder and Senior Software Engineer at TITANcode, I’m passionate about crafting innovative solutions and driving our technical vision. I lead our engineering efforts while shaping our company’s future, ensuring we deliver excellence in every project.",
+    description: "As Co-Founder and Senior Software Engineer at TITANcode, I drive innovation and technical excellence, leading our team to deliver top-quality solutions and shape our company's future.",
     positionStyle: { top: '50%', left: '62%' },
-    boxPosition: { top: '-1%', left: '20%' }
+    boxPosition: { top: '57%', left: '25%' }
   },
   {
-    name: "Expert 5",
-    position: "Position 5",
+    name: "Kithmi Sansala",
+    position: "IT Analytics",
     company: "TITANCode",
-    description: "Description for expert 5.",
+    description: "As Co-Founder and Senior Software Engineer at TITANcode, I drive innovation and technical excellence, leading our team to deliver top-quality solutions and shape our company's future.",
     positionStyle: { top: '50%', left: '75%' },
-    boxPosition: { top: '60%', left: '80%' }
+    boxPosition: { top: '57%', left: '10%' }
   }
 ];
 
@@ -57,7 +57,7 @@ const Experts = () => {
 
   return (
     <div className="bg-[#ffffff] my-[31px] mx-[31px] py-16 md:py-24 md:px-16 rounded-[50px]">
-      <h1 className="text-4xl text-[#0090FF] md:text-6xl font-bold mb-6 uppercase ">MEET THE EXPERTS</h1>
+      <h1 className="text-4xl text-[#0090FF] md:text-6xl font-bold mb-6 uppercase mx-5 ">MEET THE EXPERTS</h1>
       <div className="relative flex justify-center">
         <img src={groupImage} alt="Team" className="w-full rounded-[50px] object-bottom" />
         {experts.map((expert, index) => (
@@ -72,19 +72,20 @@ const Experts = () => {
         ))}
         {activeExpert && (
           <div
-            className="mt-6 p-4 bg-gray-100 bg-opacity-75 backdrop-blur-md rounded-lg shadow-lg absolute"
+            className=" p-2 px-4 mr-5 mt-5 bg-gray-100 bg-opacity-75 backdrop-blur-md rounded-[20px] shadow-lg absolute"
             style={{ top: activeExpert.boxPosition.top, left: activeExpert.boxPosition.left }}
           >
-            <h3 className="text-xl font-bold mb-2">{activeExpert.name}</h3>
-            <p className="text-sm font-medium text-[#0090FF] ">{activeExpert.position} at {activeExpert.company}</p>
-            <p className="mt-2">{activeExpert.description}</p>
+            <h3 className="text-[10px] font-bold sm:text-[10px] md:text-[8px] lg:text-[18px]">{activeExpert.name}</h3>
+            <p className=" font-medium text-[#0090FF] text-[8px] sm:text-[7px] md:text-[8px] lg:text-[15px] ">{activeExpert.position} at {activeExpert.company}</p>
+            <p className=" text-[8px] sm:text-[7px] md:text-[8px] lg:text-[15px]">{activeExpert.description}</p>
           </div>
         )}
-        <div className="absolute inset-x-0 bottom-5 bg-[#F4F4F5] bg-opacity-75 rounded-[50px] p-4 text-center shadow-lg w-[90%] mx-auto">
-          <p className="text-sm md:text-base">
-            At TITANCode, our team is a blend of diverse talents and expertise, each contributing to our collective strength. Our members bring a wealth of experience in technology, design, and marketing, working together to deliver innovative solutions. Our collaborative approach, commitment to excellence, and passion for problem-solving drive our success and ensure we meet and exceed client expectations.
-          </p>
-        </div>
+        {/* <div className="absolute inset-x-0 bottom-2 bg-[#F4F4F5] bg-opacity-75 rounded-[30px] mx-5 p-1 px-10 text-center shadow-lg  o">
+        <p className="text-[8px] sm:text-[7px] md:text-[8px] lg:text-[15px] ">
+            At <span className='text-[#0090FF] leading-normal font-medium'>TITANCode</span>, our diverse team excels in tech, design, and marketing, delivering innovative solutions with a collaborative and passionate approach.
+        </p>
+        </div> */}
+
       </div>
     </div>
   );
