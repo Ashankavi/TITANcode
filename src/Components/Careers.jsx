1cfcb7819ea770fd04949775dc14ca2733d2e9ba
ashan <uhashan17929@gmail.com>
2024-07-28 11:17:46 +0530

    Update

--- src/Components/Careers.jsx
+++ src/Components/Careers.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import Homeimg from "../Assets/Portfolio/rocket.png"
+import Homeimg from "../Assets/Careers/careers.png"
+import ME from "../Assets/Careers/me.png"
 
 const Careers = () => {
   return (
@@ -18,6 +19,23 @@ const Careers = () => {
         </div>
         <div className="flex justify-center order-1 mt-12 md:w-1/2 md:order-2 md:mt-0">
           <img className="" src={Homeimg} /></div>
+      </div>
+    </section>
+
+        {/* Web Section */}
+
+        <section className="bg-[#ffffff] my-[31px] mx-[31px] py-16 md:py-24 md:px-16 rounded-[50px] ">
+      <div className="max-w-8xl mx-[auto] flex flex-col md:flex-row items-center">
+      <div className="flex justify-center order-1 mx-2 mt-12 md:w-1/2 md:order-2 md:mt-0">
+          <img className="" src={ME} /> </div>
+        <div className=" bg-[#F4F4F5] py-8 md:px-16 rounded-[50px] order-2 mx-5 mt-12 md:w-1/2 md:order-2 md:mt-0 ">
+          <h1 className="text-2xl text-[#0090FF] md:text-5xl font-bold mb-4 mx-2 text-center uppercase">Message from our Leader</h1>
+          <p className="mx-2 text-[8px] md:text-[8px] lg:text-[15px]">Welcome to the TITANcode family! We're thrilled to have you on board. At TITANcode, our mission is to provide exceptional technology solutions that help businesses thrive. We are committed to delivering high-quality web development, mobile apps, UI/UX design, and digital marketing services, driving innovation and excellence in every project we undertake. Dive in, share your ideas, and let’s achieve great things together!
+          </p>
+          <p className="mx-2 my-4 text-[#4E4E4E] text-[8px] md:text-[8px] lg:text-[15px]"><span className='font-bold text-[#000000] '>Ashan Kavindu</span> <br /> Founder & CEO, TITANcode
+          </p>
+
+        </div>
       </div>
     </section>
 
--- src/Components/Talks.jsx
+++ src/Components/Talks.jsx
@@ -5,14 +5,14 @@ import { FaFacebook, FaTwitter, FaInstagram, FaPinterest, FaYoutube, FaDribbble,
 const Talks = () => {
   return (
     <div className="bg-white my-8 mx-8 py-6 md:px-16 rounded-[50px] flex flex-col lg:flex-row justify-between items-center ">
-      <div className="text-center lg:text-left mb-6 lg:mb-0">
-        <h1 className="text-[25px] sm:text-[7px] md:text-[15px] lg:text-4xl text-[#0090FF] font-bold  uppercase mx-5">
+      <div className="mb-6 text-center lg:text-left lg:mb-0">
+        <h1 className="text-[25px]  lg:text-4xl text-[#0090FF] font-bold  uppercase mx-5">
           Let's talk about how to transform your business.
         </h1>
       </div>
       <div className="flex flex-col items-center lg:items-start">
-        <p className="text-gray-600 lg:mb-1 text-center lg:text-left">Ready to build great products?</p>
-        <button className="bg-black text-white my-6 px-4 py-2 rounded-full  flex items-center hover:bg-gray-800">
+        <p className="text-center text-gray-600 lg:mb-1 lg:text-left">Ready to build great products?</p>
+        <button className="flex items-center px-4 py-2 my-6 text-white bg-black rounded-full hover:bg-gray-800">
           Let’s Talk <span className="ml-2">→</span>
         </button>
         <div className="flex space-x-4">
