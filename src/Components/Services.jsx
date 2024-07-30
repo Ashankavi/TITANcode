bb7c89dbcdf0110ec290944d4dec11415cb68e54
ashan <uhashan17929@gmail.com>
2024-07-30 17:23:40 +0530

    Update

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -9,14 +9,22 @@ import Poss from "../Assets/Services/poss.png";
 import UIUX from "../Assets/Services/uiux.png";
 import Graphic from "../Assets/Services/graphic.png";
 import Marketing from "../Assets/Services/marketing.png";
+import HomeVideo from "../Assets/Home/video.mp4";
 
 
 
 const Services = () => {
   return (
     <div>
-      <section className="bg-[#ffffff] my-[31px] mx-[31px] py-16 md:py-24 md:px-16 rounded-[50px] ">
-        <div className="flex flex-col items-center mx-auto max-w-8xl md:flex-row">
+      <section className="relative bg-[#ffffff] my-[31px] mx-[31px] py-16 md:py-24 md:px-16 rounded-[50px] overflow-hidden">
+            <video
+                autoPlay
+                loop
+                muted
+                className="absolute top-0 left-0 object-cover w-full h-full"
+                src={HomeVideo}
+            />
+        <div className="relative flex flex-col items-center mx-auto max-w-8xl md:flex-row">
           <div className="order-2 mx-5 md:w-1/2 md:order-1">
             <p className="font-medium text-blue-600">"WE ARE A LEADING PROVIDER OF TECHNOLOGY SOLUTIONS"</p>
             <h1 className="my-4 text-4xl font-bold uppercase md:text-6xl">Our Services</h1>
