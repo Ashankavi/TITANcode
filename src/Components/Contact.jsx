7bdb4af366c01a231cbaa49f7916144f0485efa2
ashan <uhashan17929@gmail.com>
2024-07-31 20:13:13 +0530

    Update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { FaFacebook, FaTwitter, FaInstagram, FaDribbble, FaBehance, FaLinkedin } from 'react-icons/fa';
+import { SiX } from 'react-icons/si';
 
 import Homeimg from "../Assets/Contact/contact.png";
-import HomeVideo from "../Assets/About/video.mp4"
+import HomeVideo from "../Assets/About/video.mp4";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -108,12 +109,12 @@ const Contact = () => {
             <div className="my-5 ">
               <h3 className="font-bold">Our Social</h3>
               <div className="flex mt-4 space-x-4">
-                <a href="#" className="text-black hover:text-[#0090FF]"><FaLinkedin size="22" /></a>
-                <a href="#" className="text-black hover:text-[#0090FF]"><FaTwitter size="22" /></a>
-                <a href="#" className="text-black hover:text-[#0090FF]"><FaFacebook size="22" /></a>
-                <a href="#" className="text-black hover:text-[#0090FF]"><FaInstagram size="22" /></a>
-                <a href="#" className="text-black hover:text-[#0090FF]"><FaDribbble size="22" /></a>
-                <a href="#" className="text-black hover:text-[#0090FF]"><FaBehance size="22" /></a>
+                <a href="https://www.linkedin.com/in/titancode" className="text-black hover:text-[#0090FF]"><FaLinkedin size="22" /></a>
+                <a href="https://x.com/titan_codesl" className="text-black hover:text-[#0090FF]"><SiX size="22" /></a>
+                <a href="https://www.facebook.com/profile.php?id=100012056560141&mibextid=LQQJ4d" className="text-black hover:text-[#0090FF]"><FaFacebook size="22" /></a>
+                <a href="https://www.instagram.com/ashxn___?igsh=b3FmZDBpemxvMTJz&utm_source=qr" className="text-black hover:text-[#0090FF]"><FaInstagram size="22" /></a>
+                <a href="https://dribbble.com/titan_codesl" className="text-black hover:text-[#0090FF]"><FaDribbble size="22" /></a>
+                <a href="https://www.behance.net/titancode" className="text-black hover:text-[#0090FF]"><FaBehance size="22" /></a>
               </div>
             </div>
           </div>
